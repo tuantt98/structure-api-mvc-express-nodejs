91c3765bd417b9345e9dce9341a1540d4858892f
tuantt98 <kisuvitinh@gmail.com>
2025-01-16 20:02:54 +0700

    Add api get users

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,11 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+router.get("/check-status", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+  });
+});
+
+router.get("/api/users", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+    metadata: [
+      {
+        name: "kuro",
+        age: 26,
+      },
+    ],
+  });
+});
 
 module.exports = router;
